2f2fcebd238461a621fab157e17ea18ed903d4ec
omid <solari@berkeley.edu>
2025-04-08 20:39:15 -0700

    add

---
+++ platipath/platipath/__init__.py
@@ -0,0 +1,11 @@
+"""
+Platinum Response Analysis - A package for analyzing somatic mutations correlated with platinum-based treatment outcomes.
+"""
+
+__version__ = "0.1.0"
+
+# Import submodules to make them available when importing platipath
+from .. import datautils
+from .. import model
+from .. import utils
+from .. import visualization
